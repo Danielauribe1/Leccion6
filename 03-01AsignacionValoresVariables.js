9c1c0acd7f9f69bc904fc46abded148ca4a8d753
DUM <114031187+Danielauribe1@users.noreply.github.com>
2022-10-18 15:07:37 -0500

    Primer avance tercer semana de octubre JS

---
+++ 03-01AsignacionValoresVariables.js
@@ -0,0 +1,9 @@
+let nombreCompleto = "Daniela Uribe";
+console.log( nombreCompleto );
+
+let x, y;
+x = 50, y = 30;
+let z = x + y;
+console.log(z);
+
+//Buenas practicas para nombres de variables en JavaScript
---
+++ 03-02ReglasNombresVariables.js
@@ -0,0 +1,13 @@
+let nombreCompleto = "Daniela Uribe";
+let nombrecompleto = "Daniel Uribe";
+console.log( nombreCompleto );
+console.log( nombrecompleto );
+//Es sensible a mayúsculas y minúsculas
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+//let 1nombreCompleto; no está permitido iniciar el nombre de una variable con números
+
+let ruptura = 10;
+
+//Tampoco se puede usar las palabras clave de JS como variables: abstract, null, public, byte, package, return, in, else, default... etc
